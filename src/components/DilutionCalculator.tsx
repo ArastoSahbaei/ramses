ae18ce8835e1d6d5491c8b8fde3b7376b8ae1eef
Arasto Sahbaei <arasto.sahbaei@gmail.com>
2023-06-17 12:22:07 +0200

    kool

--- src/components/DilutionCalculator.tsx
+++ src/components/DilutionCalculator.tsx
@@ -6,48 +6,51 @@ const Container = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  padding: 20px;
+  padding: 30px;
   font-family: Arial, sans-serif;
-  max-width: 500px;
+  max-width: 600px;
   margin: auto;
-  border-radius: 10px;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  border-radius: 15px;
+  box-shadow: 0 0 15px rgba(0, 0, 0, 0.05);
   background-color: #f9f9f9;
 `;
 
 const Title = styled.h1`
   color: #007bff;
   text-align: center;
-  margin-bottom: 20px;
+  margin-bottom: 30px;
 `;
 
 const Field = styled.div`
   width: 100%;
-  margin-bottom: 15px;
-  padding: 5px;
+  margin-bottom: 20px;
+  padding: 10px;
   background-color: #ffffff;
-  border-radius: 5px;
-  box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
+  border-radius: 10px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.05);
 `;
 
 const Label = styled.label`
   display: block;
-  margin-bottom: 5px;
+  margin-bottom: 10px;
+  font-size: 18px;
 `;
 
 const Input = styled.input`
   width: 100%;
-  padding: 5px;
-  border: 1px solid #007bff;
-  border-radius: 5px;
+  padding: 10px;
+  border: 2px solid #007bff;
+  border-radius: 10px;
+  font-size: 18px;
+  box-sizing: border-box;
 `;
 
 const Button = styled.button`
   width: 100%;
-  padding: 10px;
-  margin-top: 20px;
-  font-size: 18px;
-  border-radius: 5px;
+  padding: 15px;
+  margin-top: 30px;
+  font-size: 20px;
+  border-radius: 10px;
   background-color: #007bff;
   color: white;
   border: none;
@@ -59,13 +62,13 @@ const Button = styled.button`
 `;
 
 const Result = styled.div`
-  margin-top: 20px;
-  font-size: 20px;
+  margin-top: 30px;
+  font-size: 24px;
   color: #007bff;
-  padding: 10px;
+  padding: 15px;
   background-color: #ffffff;
-  border-radius: 5px;
-  box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
+  border-radius: 10px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.05);
   width: 100%;
   text-align: center;
 `;
