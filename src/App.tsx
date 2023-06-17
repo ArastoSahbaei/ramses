b7a64eb690c9e56125b44a97ecd7931eb85f5333
Arasto Sahbaei <arasto.sahbaei@gmail.com>
2023-06-17 12:26:35 +0200

    ok

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
+import React from "react";
+import styled from "styled-components";
 import DilutionCalculator from "./components/DilutionCalculator";
+
+const CenteredDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh; /* Adjust this value based on your desired height */
+`;
 
 export const App = () => {
   return (
-    <div>
+    <CenteredDiv>
       <DilutionCalculator />
-    </div>
+    </CenteredDiv>
   );
 };
